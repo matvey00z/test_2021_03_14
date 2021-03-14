93a453cf46010541e132118435cae241ed70d74f
Matvey Zaharchenko <matvey.ff@gmail.com>
2021-03-14 21:59:30 +0300

    Cleanup
    Remove slow versions and profiling

--- count_nice_numbers.cpp
+++ count_nice_numbers.cpp
@@ -1,153 +1,10 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
+#include <cstdint>
+#include <cstdio>
 
 #include <vector>
-#include <string>
-#include <chrono>
-#include <functional>
 
 constexpr int halflen = 6;
 constexpr int base = 13;
-
-
-uint64_t count(int leftsum, int rightsum, int leftlen, int rightlen)
-{
-	if(leftlen == 0)
-	{
-		if(rightlen == 0)
-			return leftsum == rightsum ? 1 : 0;
-		uint64_t ret = 0;
-		for(int d = 0; d < base; ++d)
-			ret += count(leftsum, rightsum + d, leftlen, rightlen - 1);
-		return ret;
-	}
-	uint64_t ret = 0;
-	for(int d = 0; d < base; ++d)
-		ret += count(leftsum + d, rightsum, leftlen - 1, rightlen);
-	return ret;
-}
-
-uint64_t recoursive()
-{
-	return count(0, 0, halflen, halflen) * base;
-}
-
-class Number
-{
-	public:
-		Number()
-		{
-			memset(number, 0, len);
-		}
-		static Number max()
-		{
-			Number m;
-			memset(m.number, base - 1, len);
-			return m;
-		}
-		bool is_nice()
-		{
-			int left = 0;
-			int right = 0;
-			for(int i = 0; i < halflen; ++i)
-			{
-				left += number[i];
-				right += number[halflen + i];
-			}
-			return left == right;
-		}
-		friend bool operator!=(const Number &l, const Number &r)
-		{
-			return memcmp(l.number, r.number, len);
-		}
-		Number &operator++()
-		{
-			int add = 1;
-			for(int i = len - 1; i >= 0; --i)
-			{
-				number[i] += add;
-				add = number[i] / base;
-				number[i] %= base;
-			}
-			return *this;
-		}
-	private:
-		static constexpr int len{2*halflen};
-		uint8_t number[len];
-};
-
-uint64_t bruteforce()
-{
-	Number max = Number::max();
-	uint64_t count = 0;
-	for(Number n; n != max; ++n)
-	{
-		if(n.is_nice())
-			++count;
-	}
-	if(max.is_nice())
-		++count;
-	return count * base;
-}
-
-
-void add_sums(std::vector<uint64_t> &sums_count, int prefix_sum, int len)
-{
-	if(len == 0)
-		sums_count[prefix_sum]++;
-	else
-	{
-		for(int d = 0; d < base; ++d)
-			add_sums(sums_count, prefix_sum + d, len - 1);
-	}
-}
-
-uint64_t opt3()
-{
-	constexpr int max_sum = halflen * (base - 1);
-	std::vector<uint64_t> sums_count(max_sum + 1, 0);
-	add_sums(sums_count, 0, halflen);
-	uint64_t count = 0;
-	for(auto &c: sums_count)
-	{
-		count += c * c;
-	}
-	return count * base;
-}
-
-int sum_digits(int num)
-{
-	int sum = 0;
-	while(num)
-	{
-		sum += num % base;
-		num /= base;
-	}
-	return sum;
-}
-
-constexpr int get_max_half()
-{
-	int ret = 1;
-	for(int i = 0; i < halflen; ++i)
-		ret *= base;
-	return ret;
-}
-
-uint64_t opt4()
-{
-	int max_half = get_max_half();
-	uint64_t count = 0;
-	for(int left = 0; left < max_half; ++left)
-	{
-		for(int right = 0; right < max_half; ++right)
-			if(sum_digits(left) == sum_digits(right))
-				++count;
-	}
-	return count * base;
-}
-
 
 constexpr uint64_t factorial(int n)
 {
@@ -188,21 +45,8 @@ uint64_t opt5()
 	return count * base;
 }
 
-void profile(const std::string &name, const std::function<uint64_t()> &f)
-{
-	auto before = std::chrono::steady_clock::now();
-	auto ret = f();
-	auto after = std::chrono::steady_clock::now();
-	printf("%s: %lu %.3lf s.\n", name.c_str(), ret,
-		       	std::chrono::duration_cast<std::chrono::milliseconds>(after - before).count()/1e3);
-}
-
-#define PROFILE(f) do { profile(#f, f); } while(0)
 int main(int, char**)
 {
-	PROFILE(opt5);
-	PROFILE(opt3);
-	PROFILE(recoursive);
-	PROFILE(bruteforce);
+	printf("%lu\n", opt5());
 	return 0;
 }
